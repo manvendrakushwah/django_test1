46fef35ae1410459b9e0bdd7c309a9bfd6003326
manvendrakushwah <mskushwah345@gmail.com>
2018-12-12 15:36:38 +0530

    Index M also changes

--- music/views.py
+++ music/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
+
 
 def index(request):
-    return HttpResponse("<h1>This is the music Homepage" );
+    all_albums=Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+
+    return HttpResponse(template.render(context,request))
+
+
+
+
+
+
+
